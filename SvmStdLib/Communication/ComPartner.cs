75b91ad35ff6b15ac577201abe8142475e0bfe81
Martin Kramer <mail@martinkramer.dk>
2021-01-15 09:29:18 +0100

    lsdkfjsdklj

---
+++ SvmStdLib/Communication/ComPartner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvmStdLib.Communication
+{
+    public class ComPartner
+    {
+
+    }
+}
